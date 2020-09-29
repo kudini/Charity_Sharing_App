6ba288750a1624f9522c8eb1ad62eeb1b494c05a
Jerzy Pałka <57533018+kudini@users.noreply.github.com>
2020-09-29 16:23:36 +0200

    Basic Implementation of managing AdminController

--- src/main/java/pl/coderslab/charity/user/controller/AdminController.java
+++ src/main/java/pl/coderslab/charity/user/controller/AdminController.java
@@ -6,52 +6,55 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import pl.coderslab.charity.donation.dto.DonationDto;
 import pl.coderslab.charity.user.dto.UserDto;
 import pl.coderslab.charity.user.entity.Role;
 import pl.coderslab.charity.user.entity.User;
 import pl.coderslab.charity.user.service.UserService;
 
-import java.util.HashSet;
-
 @Controller
 public class AdminController {
-    private  UserService userService;
     private final Role ADMIN_ROLE = new Role("ROLE_ADMIN");
+    private UserService userService;
 
     public AdminController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping("/admin/panel")
-    public String listAdminsPanel(){
+    public String listAdminsPanel() {
         return "admin-panel-list";
     }
+
     @GetMapping("/admin/panel/add")
-    public String adminPanelAddGet(){
+    public String adminPanelAddGet() {
         return "admin-add";
     }
+
     @PostMapping("/admin/panel/add")
-    public String adminPanelAddPost(){
+    public String adminPanelAddPost() {
         return "redirect:/admin/panel";
     }
+
     @GetMapping("/admin/{id}/edit")
-    public String adminPanelEditGet(@PathVariable Long id, Model model){
-        boolean isAdmin=false;
+    public String adminPanelEditGet(@PathVariable Long id, Model model) {
+        boolean isAdmin = false;
         UserDto userDto = new UserDto(userService.findById(id));
-        model.addAttribute("userDto",userDto);
-        if(userDto.getRoles().contains(ADMIN_ROLE)){
-            isAdmin=true;
+        model.addAttribute("userDto", userDto);
+        if (userDto.getRoles().contains(ADMIN_ROLE)) {
+            isAdmin = true;
         }
-        model.addAttribute("isAdmin",isAdmin);
+        model.addAttribute("isAdmin", isAdmin);
         return "admin-add";
     }
+
     @PostMapping("/admin/{id}/edit")
-    public String adminPanelEditPost(@ModelAttribute UserDto userDto){
+    public String adminPanelEditPost(@ModelAttribute UserDto userDto) {
 
         return "redirect:/admin/panel";
     }
+
     @GetMapping("/admin/{id}/delete")
-    public String adminPanelDelete(@PathVariable Long id){
+    public String adminPanelDelete(@PathVariable Long id) {
         User user = userService.findById(id);
         user.getRoles().remove(ADMIN_ROLE);
         userService.saveUser(user);
